1a46a983b407d677cf4ddcc229875b6ddc0303d0
Mohannad Hisham <Mohannadize15@gmail.com>
2023-12-01 15:00:13 +0200

    Database timezone

--- lib/database.php
+++ lib/database.php
@@ -1,33 +1,65 @@
 <?php
 
-$db = new PDO("sqlite:sql.db");
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 class Database
 {
+    private static $db = "sqlite:sql.db";
+    private static $connection;
+
+    static function init()
+    {
+        self::$connection = new PDO(self::$db);
+        self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        self::set_timezone("Africa/Abidjan");
+    }
+
     static function query(string $sql)
     {
-        global $db;
-        $res = $db->query($sql);
+        $res = self::$connection->query($sql);
         $res->setFetchMode(PDO::FETCH_ASSOC);
         return $res;
     }
 
+    static function auto_commit(bool $status)
+    {
+        return self::$connection->setAttribute(PDO::ATTR_AUTOCOMMIT, $status);
+    }
+
     static function prepare(string $sql)
     {
-        global $db;
-        return $db->prepare($sql);
+        return self::$connection->prepare($sql);
     }
 
     static function begin_transaction()
     {
-        global $db;
-        return $db->beginTransaction();
+        self::auto_commit(false);
+        return self::$connection->beginTransaction();
     }
 
     static function commit()
     {
-        global $db;
-        return $db->commit();
+        try {
+            $res = self::$connection->commit();
+            self::auto_commit(true);
+            return $res;
+        } catch (\Throwable $th) {
+            self::$connection->rollBack();
+            self::auto_commit(true);
+            throw $th;
+        }
+    }
+
+    static function set_timezone($timezone)
+    {
+        define('TIMEZONE', $timezone);
+        date_default_timezone_set($timezone);
+        $now = new DateTime();
+        $mins = $now->getOffset() / 60;
+        $sgn = ($mins < 0 ? -1 : 1);
+        $mins = abs($mins);
+        $hrs = floor($mins / 60);
+        $mins -= $hrs * 60;
+        $offset = sprintf('%+d:%02d', $hrs*$sgn, $mins);
+        self::query("SET time_zone='$offset'");
+        return true;
     }
 }
